dad5790ac1f380774073e2616d8b0de24cfe385c
Min Min Thway <minthway668@gmail.com>
2022-04-21 15:33:50 +0630

    vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import JobView from '../views/job/JobView.vue'
+import JobDetailView from '../views/job/JobDetailView.vue'
 
 const routes = [
   {
@@ -14,6 +16,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path :'/job',
+    name : "job",
+    component : JobView
+  },
+  {
+    path :'/job/:id',
+    name : 'jobdetail',
+    component : JobDetailView,
+    props : true,
   }
 ]
 
---
+++ src/views/job/JobView.vue
@@ -0,0 +1,38 @@
+<template>
+  <h3>This is jobs</h3>
+  <div v-for="data in datas" :key="data.id">
+        <router-link :to="{name:'jobdetail', params:{id:data.id}}">
+            {{ data.name }}
+        </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            "datas" : [
+                {
+                    id : "1",
+                    name : "Min Min Thwya",
+                    job : "Developer"
+                },
+                 {
+                    id : "2",
+                    name : "Min Thwya",
+                    job : "Designer"
+                },
+                 {
+                    id : "3",
+                    name : "Aung Min THway",
+                    job : "Moblie"
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
